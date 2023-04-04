abb802470db08d1c7ca54c2e0d0da40f61f7c210
Matthewmfl <735518@student.dusd.net>
2023-04-04 09:58:45 -0700

    Challenge
    Completed the challenge

--- Unit3SoundAndEffectFlorezLawson/Assets/Scripts/PlayerController.cs
+++ Unit3SoundAndEffectFlorezLawson/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,7 @@ public class PlayerController : MonoBehaviour
     private AudioSource playerAudio;
     public bool doubleJumpUsed = false;
     public float doubleJumpForce;
+    public bool doubleSpeed = false;
 
 
     // Start is called before the first frame update
@@ -41,12 +42,22 @@ public class PlayerController : MonoBehaviour
 
             doubleJumpUsed = false;
         } 
-        else if(Input.GetKeyDown(KeyCode.Space)&& !isOnGround && !doubleJumpUsed)
+        else if(Input.GetKeyDown(KeyCode.Space) && !isOnGround && !doubleJumpUsed)
         {
             doubleJumpUsed = true;
             playerRb.AddForce(Vector3.up * doubleJumpForce, ForceMode.Impulse);
-            playerAnim.Play("Running_Jump", 3.0f);
+            playerAnim.Play("Running_Jump", 3, 0f);
             playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            doubleSpeed = true;
+            playerAnim.SetFloat("Speed_Multiplier", 2.0f);
+        }
+        else if (doubleSpeed)
+        {
+            doubleSpeed = false;
+            playerAnim.SetFloat("Speed_Multiplier", 1.0f);
         }
     }
     private void OnCollisionEnter(Collision collision)
